dd60c75196f1c5ea729354a3fe3221428f0efc11
Hady Jaafar <hady.jaafar@polymtl.ca>
2021-04-22 13:17:47 -0400

    include delay retirer

--- Echiquier.cpp
+++ Echiquier.cpp
@@ -11,16 +11,6 @@
 #include "Cavalier.h"
 #include "Tour.h"
 #include "Echiquier.h"
-# ifndef WIN32_LEAN_AND_MEAN
-#   define WIN32_LEAN_AND_MEAN
-# endif
-# ifndef VC_EXTRALEAN
-#   define VC_EXTRALEAN
-# endif
-# ifndef NOMINMAX
-#   define NOMINMAX
-# endif
-#include <Windows.h>
 using namespace std;
 
 
